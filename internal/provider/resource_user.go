5cec8a0d07ba8629d2d658ada5a25fca1d320ef4
flawless <alushanov92@gmail.com>
2025-05-05 09:40:30 +0400

    refactor: update UserResource implementation with example schema and improved structure

--- internal/provider/resource_user.go
+++ internal/provider/resource_user.go
@@ -1,153 +1,196 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package provider
 
 import (
-   "context"
-   "fmt"
-   "net/http"
+	"context"
+	"fmt"
+	"net/http"
 
-   "github.com/hashicorp/terraform-plugin-framework/path"
-   "github.com/hashicorp/terraform-plugin-framework/resource"
-   "github.com/hashicorp/terraform-plugin-framework/resource/schema"
-   "github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-   "github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
-   "github.com/hashicorp/terraform-plugin-framework/types"
-
-   // your shared metadata helpers
-   "github.com/hashicorp/billingbox/internal/provider/schema_metadata"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
-// UserResource implements the Terraform resource for User.
-type UserResource struct {
-   client *http.Client
-}
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &UserResource{}
+var _ resource.ResourceWithImportState = &UserResource{}
 
-// NewUserResource returns a UserResource.
 func NewUserResource() resource.Resource {
-   return &UserResource{}
+	return &UserResource{}
 }
 
-// UserResourceModel describes the User resource data model.
-type UserResourceModel struct {
-   Id        types.String         `tfsdk:"id"`
-   UserName  types.String         `tfsdk:"user_name"`
-   Active    types.Bool           `tfsdk:"active"`
-   Locale    types.String         `tfsdk:"locale"`
-   TwoFactor types.Bool           `tfsdk:"two_factor"`
-   // … add other top‐level attributes here …
+// UserResource defines the resource implementation.
+type UserResource struct {
+	client *http.Client
+}
 
-   Metadata  schema_metadata.MetadataModel `tfsdk:"metadata"`
+type UserName struct {
+	GivenName       string `json:"givenName,omitempty"`
+	MiddleName      string `json:"middleName,omitempty"`
+	FamilyName      string `json:"familyName,omitempty"`
+	HonorificPrefix string `json:"honorificPrefix,omitempty"`
+}
+
+// UserResourceModel describes the resource data model.
+type UserResourceModel struct {
+	ResourceType      string              `json:"resourceType"`
+	ID                string              `json:"id,omitempty"`
+	UserName          string              `json:"userName"`
+	Password          string              `json:"password,omitempty"`
+	Name              *UserName           `json:"name,omitempty"`
+	Meta              *Meta               `json:"meta,omitempty"`
 }
 
 func (r *UserResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-   resp.TypeName = req.ProviderTypeName + "_user"
+	resp.TypeName = req.ProviderTypeName + "_example"
 }
 
 func (r *UserResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
-   resp.Schema = schema.Schema{
-      MarkdownDescription: "User resource",
-      Attributes: map[string]schema.Attribute{
-         "id": schema.StringAttribute{
-            Computed:            true,
-            MarkdownDescription: "Unique identifier for the User",
-            PlanModifiers: []planmodifier.String{
-               stringplanmodifier.UseStateForUnknown(),
-            },
-         },
-         "user_name": schema.StringAttribute{
-            Required:            true,
-            MarkdownDescription: "Unique username of the User",
-         },
-         "active": schema.BoolAttribute{
-            Optional:            true,
-            MarkdownDescription: "Indicates the User's administrative status",
-         },
-         "locale": schema.StringAttribute{
-            Optional:            true,
-            MarkdownDescription: "Default localization setting for the User",
-         },
-         "two_factor": schema.BoolAttribute{
-            Optional:            true,
-            MarkdownDescription: "Two‐factor settings",
-         },
-         // … define additional attributes as needed …
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Aidbox user resource",
 
-         // shared metadata nested block
-         "metadata": schema.SingleNestedAttribute{
-            Computed:            true,
-            MarkdownDescription: "Server‐side resource metadata",
-            Attributes:         schema_metadata.MetadataAttributes(),
-            PlanModifiers: []planmodifier.Object{
-               planmodifier.SuppressChanges(), // no diff on metadata
-            },
-         },
-      },
-   }
+		Attributes: map[string]schema.Attribute{
+			"configurable_attribute": schema.StringAttribute{
+				MarkdownDescription: "Example configurable attribute",
+				Optional:            true,
+			},
+			"defaulted": schema.StringAttribute{
+				MarkdownDescription: "Example configurable attribute with default value",
+				Optional:            true,
+				Computed:            true,
+				Default:             stringdefault.StaticString("example value when not configured"),
+			},
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Example identifier",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+		},
+	}
 }
 
 func (r *UserResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
-   if req.ProviderData == nil {
-      return
-   }
-   client, ok := req.ProviderData.(*http.Client)
-   if !ok {
-      resp.Diagnostics.AddError("Unexpected Resource Configure Type",
-         fmt.Sprintf("Expected *http.Client, got: %T", req.ProviderData))
-      return
-   }
-   r.client = client
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*http.Client)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = client
 }
 
 func (r *UserResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-   var plan UserResourceModel
-   resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
-   if resp.Diagnostics.HasError() {
-      return
-   }
-   // TODO: call your API to create the User, parse response
-   var state UserResourceModel
-   state.Id = types.StringValue("generated-id")
-   state.UserName = plan.UserName
-   state.Active = plan.Active
-   state.Locale = plan.Locale
-   state.TwoFactor = plan.TwoFactor
-   state.Metadata = schema_metadata.MetadataFromResponse(/* API‐response metadata */)
-   resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+	var data UserResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to create example, got error: %s", err))
+	//     return
+	// }
+
+	// For the purposes of this example code, hardcoding a response value to
+	// save into the Terraform state.
+	data.Id = types.StringValue("example-id")
+
+	// Write logs using the tflog package
+	// Documentation: https://terraform.io/plugin/log
+	tflog.Trace(ctx, "created a resource")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *UserResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-   var state UserResourceModel
-   resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
-   if resp.Diagnostics.HasError() {
-      return
-   }
-   // TODO: call your API to refresh the User by state.Id
-   resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+	var data UserResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read example, got error: %s", err))
+	//     return
+	// }
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *UserResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-   var plan UserResourceModel
-   resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
-   if resp.Diagnostics.HasError() {
-      return
-   }
-   // TODO: call your API to update the User
-   var state UserResourceModel
-   state.Id = plan.Id
-   state.UserName = plan.UserName
-   // … copy other fields …
-   state.Metadata = schema_metadata.MetadataFromResponse(/* API‐response metadata */)
-   resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+	var data UserResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to update example, got error: %s", err))
+	//     return
+	// }
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *UserResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-   var state UserResourceModel
-   resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
-   if resp.Diagnostics.HasError() {
-      return
-   }
-   // TODO: call your API to delete the User by state.Id
+	var data UserResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to delete example, got error: %s", err))
+	//     return
+	// }
 }
 
 func (r *UserResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-   resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
 }
